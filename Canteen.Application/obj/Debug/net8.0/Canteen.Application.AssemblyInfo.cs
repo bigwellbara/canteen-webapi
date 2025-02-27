8c6345a957069de7dad58cc1d0cebf1afd48129e
bigwell <bbigwel@gmail.com>
2025-02-27 16:35:30 +0200

    installing required NuGet packages

--- Canteen.Application/obj/Debug/net8.0/Canteen.Application.AssemblyInfo.cs
+++ Canteen.Application/obj/Debug/net8.0/Canteen.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Canteen.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+172139848c5d15d6ae2922c192228b34409ab5f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Canteen.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Canteen.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Canteen.Domain/obj/Debug/net8.0/Canteen.Domain.AssemblyInfo.cs
+++ Canteen.Domain/obj/Debug/net8.0/Canteen.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Canteen.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+172139848c5d15d6ae2922c192228b34409ab5f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Canteen.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Canteen.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
